9705974d9ffc38f373dd6c8509f7f5393f944fa3
requenaGuillaume <89648307+requenaGuillaume@users.noreply.github.com>
2021-09-14 15:51:14 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+// *************************************************************************************************************************** \\
+// ****************************************************** VARIABLES ********************************************************** \\
+// *************************************************************************************************************************** \\
+const carrousels = document.querySelectorAll("main article:nth-of-type(1) div");
+const titles = document.querySelectorAll("h2");
+const articles = document.querySelectorAll("article");
+const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+let progress; // Scroll bar progression in %
+
+// *************************************************************************************************************************** \\
+// ****************************************************** MAIN CODE ********************************************************** \\
+// *************************************************************************************************************************** \\
+window.addEventListener("DOMContentLoaded", function () {
+  window.document.addEventListener("scroll", function () {
+    progress = (Math.ceil(window.scrollY) * 100) / scrollable;
+
+    console.log(progress);
+
+    // First h2 & article
+    showTitle(2, 0);
+    showArticle(3, 0);
+
+    showTitle(28, 1);
+    showArticle(29, 1);
+
+    showTitle(31, 2);
+    showArticle(33, 2);
+
+    showTitle(38, 3);
+    showArticle(39, 3);
+
+    showTitle(44, 4);
+    showArticle(45, 4);
+
+    showTitle(50, 5);
+    showArticle(52, 5);
+  });
+
+  carrousel(carrousels[0], "firstCarrousel");
+  carrousel(carrousels[1], "secondCarrousel");
+  carrousel(carrousels[2], "thirdCarrousel");
+  carrousel(carrousels[3], "fourthCarrousel");
+});
+
+// *************************************************************************************************************************** \\
+// ****************************************************** FUNCTIONS ********************************************************** \\
+// *************************************************************************************************************************** \\
+function carrousel(element, carrouselNumber) {
+  element.addEventListener("mouseover", function () {
+    this.classList.add(carrouselNumber);
+  });
+
+  element.addEventListener("mouseout", function () {
+    this.classList.remove(carrouselNumber);
+  });
+}
+
+function showTitle(progressPercentage, number) {
+  if (
+    progress >= progressPercentage &&
+    !titles[number].classList.contains("show-right")
+  ) {
+    titles[number].classList.add("show-right");
+  }
+}
+
+function showArticle(progressPercentage, number) {
+  if (
+    progress >= progressPercentage &&
+    !articles[number].classList.contains("show-left")
+  ) {
+    articles[number].classList.add("show-left");
+  }
+}
